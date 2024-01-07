3e0d7f37d76f3b16b5cd7239534d67b3aaf7474a
Qunzhang1996 <112466735+Qunzhang1996@users.noreply.github.com>
2024-01-07 15:57:57 +0000

    update point out

--- rearrange_ws/src/rearrange/scripts/navigation_node.py
+++ rearrange_ws/src/rearrange/scripts/navigation_node.py
@@ -7,9 +7,13 @@ from geometry_msgs.msg import PoseStamped
 from rearrange.msg import NavigateDrinkAction, NavigateDrinkFeedback, NavigateDrinkResult
 import json
 import tf
+import math
+import subprocess
 
 file_path = '/home/user/exchange/ssy236_project/rearrange_ws/src/rearrange/queries/object_pose.json'
-
+cur_coke_command =  "rosrun tiago_moveit_tutorial plan_arm_torso_fk  0.35 0.4 0.3 -1.7 1.5 -1.2 1.2 0"
+cur_beer_command =  "rosrun tiago_moveit_tutorial plan_arm_torso_fk  0.25 0.4 -0.2 -1.7 1.5 -1.2 1.2 0"
+dst_command = "rosrun tiago_moveit_tutorial plan_arm_torso_fk  0.27 0.4 -0.3 -1.7 1.5 -1.5 1.14 0"
 def load_json(file_path):
     try:
         with open(file_path, 'r') as json_file:
@@ -27,16 +31,25 @@ def get_pose(data, object_name):
             return object_pose, expected_location_pose
     return None, None
 
+#! step1: move to expand the arm 
 def adjust_pose(pose):
     x, y, z, w = pose['position']['x'], pose['position']['y'], pose['orientation']['z'], pose['orientation']['w']
     if y > 0:
-        x -= 0.5
+        x -= 1
     else:
-        x += 1
+        x += 1.1
         original_quaternion = [0, 0, z, w]
         rotation_quaternion = [0, 0, 1, 0]
         new_quaternion = tf.transformations.quaternion_multiply(original_quaternion, rotation_quaternion)
         z, w = new_quaternion[2], new_quaternion[3]
+    return x, y, z, w
+#! step2: move to take the drink
+def adjust_pose_step2(pose):
+    x, y, z, w = pose['position']['x'], pose['position']['y'], pose['orientation']['z'], pose['orientation']['w']
+    if y > 0:
+        x -= 0.5
+    else:
+        x += 0.6
     return x, y, z, w
 
 
@@ -55,9 +68,18 @@ def handle_navigate_drink(goal, client, data):
     if drink_pose:
         send_goal(client, adjust_pose(drink_pose))
         rospy.loginfo("Reached the drink location.")
+        #! here, move the robot to take the drink
+        if goal.drink_name == 'coke':
+            subprocess.run(cur_coke_command, shell=True)
+        elif goal.drink_name == 'beer':
+            subprocess.run(cur_beer_command, shell=True)
+        send_goal(client, adjust_pose_step2(drink_pose))
+        rospy.loginfo("Took the drink.")
     if expected_location:
         rospy.loginfo("Navigating to the expected location.")
         send_goal(client, adjust_pose(expected_location))
+        #! PUT THE DRINK DOWN
+        subprocess.run(dst_command, shell=True)
         rospy.loginfo("Reached the expected location.")
 
 if __name__ == '__main__':
